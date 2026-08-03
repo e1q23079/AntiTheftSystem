d1d7dfc27d3bfe426dca5de5890803b674951975
e1q23079 <e1q23079@oit.ac.jp>
2026-08-04 02:08:29 +0900

    テスト関数を整理し、ライブラリとサーバーのテストを明確に分離

--- tools/test.py
+++ tools/test.py
@@ -1,16 +1,23 @@
 import unittest
 
 
-def main():
+def test_lib():
     loader = unittest.TestLoader()
     suite = loader.discover("lib/tests", pattern="test_*.py")
     runner = unittest.TextTestRunner(verbosity=2)
     runner.run(suite)
 
+
+def test_server():
     loader = unittest.TestLoader()
     suite = loader.discover("tests", pattern="test_*.py")
     runner = unittest.TextTestRunner(verbosity=2)
     runner.run(suite)
+
+
+def main():
+    test_lib()
+    test_server()
 
 
 if __name__ == "__main__":
